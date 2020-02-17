26e7e2fe1cb0abfc2c9b3368955d1bf97ddbf561
Camden Davis <camdendavis99@ksu.edu>
2020-02-17 16:35:03 -0600

    Create basic project structure

---
+++ model.py
@@ -0,0 +1,11 @@
+class Model:
+
+    def __init__(self, genre: str):
+        """Creates a new Magenta model for the given genre of music"""
+        self.genre = genre
+
+    def train_model(self):
+        pass
+
+    def compose_song(self):
+        pass
